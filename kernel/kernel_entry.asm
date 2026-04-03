c44f6ad3b87c967226ba558877147996cfa4ea72
Amai <zhengcheng011@gmail.com>
2026-04-03 22:01:37 +0800

    recodeeee

--- kernel/kernel_entry.asm
+++
@@ -1,9 +0,0 @@
-[BITS 32]           ; We enter 32-bit protected mode here
-
-[EXTERN kernel_main] ; Defined in kernel.c
-
-global _start
-
-_start:
-    call kernel_main
-    hlt             ; Should never reach here
